a48275fd9c664644f2263559edb3897b87dc804a
EC2 Default User <ec2-user@ip-172-31-22-73.ec2.internal>
2021-12-24 20:08:42 +0000

    try detacting visiable element

--- ProjectC/image_display_m.js
+++ ProjectC/image_display_m.js
@@ -1,4 +1,7 @@
 /* global d3 */
+/* global jQuery */
+/* global $ */
+
 var imageFile = './image.json';
 
 if (window.innerWidth > window.innerHeight) {
@@ -65,7 +68,7 @@ function displayImages(data, root, startDate, endDate){
             // add to our filename first
             return './downloads/' + d.filename;
         })
-       .on("mouseover", (event, itemData) => {
+       .on("touchstart", (event, itemData) => {
             //console.log(itemData.title);
             var title = document.getElementById("title");
             title.innerHTML=itemData.title;
@@ -138,3 +141,58 @@ function onNavClick() {
   hideOverlay();
   return true;
 }
+
+
+//try detecting visiable element on screen
+function isElementInViewport (el) {
+
+    // Special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+    );
+}
+
+
+function onVisibilityChange(el, callback) {
+    var old_visible;
+    return function () {
+        var visible = isElementInViewport(el);
+        if (visible != old_visible) {
+            old_visible = visible;
+            if (typeof callback == 'function') {
+                callback();
+            }
+        }
+    }
+}
+
+var handler = onVisibilityChange(el, function() {
+    /* Your code go here */
+});
+
+
+// jQuery
+$(window).on('DOMContentLoaded load resize scroll', handler);
+
+/* // Non-jQuery
+if (window.addEventListener) {
+    addEventListener('DOMContentLoaded', handler, false);
+    addEventListener('load', handler, false);
+    addEventListener('scroll', handler, false);
+    addEventListener('resize', handler, false);
+} else if (window.attachEvent)  {
+    attachEvent('onDOMContentLoaded', handler); // Internet Explorer 9+ :(
+    attachEvent('onload', handler);
+    attachEvent('onscroll', handler);
+    attachEvent('onresize', handler);
+}
+*/
